7ccd54ecae478375b427c87ceaa68a1e5efed25b
Ross King <rlking1985@gmail.com>
2021-10-05 20:52:43 +1100

    Working on get list items

---
+++ src/services/lists/getListItems.test.ts
@@ -0,0 +1,7 @@
+import getListItems from "./getListItems";
+
+describe("getListItems", () => {
+  it("getListItems has no errors", async () => {
+    const res = await getListItems("Get Last Item ID");
+  });
+});
