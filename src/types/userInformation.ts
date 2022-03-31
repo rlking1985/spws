a8b81e4f49fb489042f94c6d0572ccef79ef667f
Ross King <rlking1985@gmail.com>
2022-03-31 21:04:27 +1100

    Added src to npm

--- src/types/userInformation.ts
+++ src/types/userInformation.ts
@@ -4,22 +4,6 @@ type Deferred = {
   };
 };
 
-// <d:ContentTypeID>0x010A00891D6A2DE286304CB902CD2350188141</d:ContentTypeID>
-// <d:Name>DRN\Administrator</d:Name>
-// <d:Account>DRN\administrator</d:Account>
-// <d:IsSiteAdmin m:type="Edm.Boolean">true</d:IsSiteAdmin>
-// <d:Deleted m:type="Edm.Boolean">false</d:Deleted>
-// <d:UserName>Administrator</d:UserName>
-// <d:Id m:type="Edm.Int32">1</d:Id>
-// <d:ContentType>Person</d:ContentType>
-// <d:Modified m:type="Edm.DateTime">2021-10-30T00:00:03</d:Modified>
-// <d:Created m:type="Edm.DateTime">2018-11-05T14:22:31</d:Created>
-// <d:CreatedById m:type="Edm.Int32">1</d:CreatedById>
-// <d:ModifiedById m:type="Edm.Int32">1073741823</d:ModifiedById>
-// <d:Owshiddenversion m:type="Edm.Int32">67</d:Owshiddenversion>
-// <d:Version>1.0</d:Version>
-// <d:Path>/army/wii/_catalogs/users</d:Path>
-
 type UserInformation = {
   AboutMe?: string;
   Account: string;
